4d500e59f61dbfa033fe6e79722b5f4fe67804d5
Dev <dev@dev.sp-tarkov.com>
2023-08-08 16:51:42 +0100

    Update config file loader to check for and process json5 files
    Add functionality to jsonUtil to parse/strigify json5 data
    
    Refactor of deserializeWithCacheCheck() that turns out not to be necessary

--- project/src/utils/JsonUtil.ts
+++ project/src/utils/JsonUtil.ts
@@ -1,6 +1,7 @@
 import fixJson from "json-fixer";
 import { inject, injectable } from "tsyringe";
 
+import json5 from "json5";
 import { ILogger } from "../models/spt/utils/ILogger";
 import { HashUtil } from "./HashUtil";
 import { VFS } from "./VFS";
@@ -10,6 +11,7 @@ export class JsonUtil
 {
     protected fileHashes = null;
     protected jsonCacheExists = false;
+    protected jsonCachePath = "./user/cache/jsonCache.json";
 
     constructor(
         @inject("VFS") protected vfs: VFS,
@@ -49,8 +51,47 @@ export class JsonUtil
     }
 
     /**
+     * From object to string
+     * @param data object to turn into JSON
+     * @param replacer An array of String and Number objects that serve as a
+    * allowlist for selecting/filtering the properties of the value object to be
+    * included in the JSON5 string. If this value is null or not provided, all
+    * properties of the object are included in the resulting JSON5 string.
+    * @param space A String or Number object that's used to insert white space into
+    * the output JSON5 string for readability purposes. If this is a Number, it
+    * indicates the number of space characters to use as white space; this number
+    * is capped at 10 (if it is greater, the value is just 10). Values less than 1
+    * indicate that no space should be used. If this is a String, the string (or
+    * the first 10 characters of the string, if it's longer than that) is used as
+    * white space. If this parameter is not provided (or is null), no white space
+    * is used. If white space is used, trailing commas will be used in objects and
+    * arrays.
+    * @param space A String representing the quote character to use when serializing
+    * @param filename Name of file being serialized
+    * @returns The JSON5 string converted from the JavaScript value.
+     */
+    public serializeJson5(
+        data: any,
+        replacer?: | ((this: any, key: string, value: any) => any) | (string | number)[] | null,
+        space?: string | number | null,
+        quote?: string | null,
+        filename?: string | null): string
+    {
+        try
+        {
+            return json5.stringify(data, {replacer, space, quote});
+        }
+        catch (error)
+        {
+            this.logger.error(`unable to stringify json5 file: ${filename} message: ${error.message}, stack: ${error.stack}`);
+        }
+        
+    }
+
+    /**
      * From string to object
      * @param jsonString json string to turn into object
+     * @param filename Name of file being deserialized
      * @returns object
      */
     public deserialize<T>(jsonString: string, filename = ""): T
@@ -64,6 +105,27 @@ export class JsonUtil
         return data;
     }
 
+    /**
+     * From string to object
+     * @param jsonString json string to turn into object
+     * @param filename Name of file being deserialized
+     * @param reviver A function that prescribes how the value originally produced
+     * by parsing is transformed before being returned.
+     * @returns object
+     */
+    public deserializeJson5<T>(jsonString: string, filename = "", reviver?: ((this: any, key: string, value: any) => any) | null): T
+    {
+        try
+        {
+            return json5.parse(jsonString, reviver);
+        }
+        catch (error)
+        {
+            this.logger.error(`unable to parse json5 file: ${filename} message: ${error.message}, stack: ${error.stack}`);
+        }
+        
+    }
+
     public async deserializeWithCacheCheckAsync<T>(jsonString: string, filePath: string): Promise<T>
     {
         return new Promise((resolve) => 
@@ -74,27 +136,11 @@ export class JsonUtil
 
     public deserializeWithCacheCheck<T>(jsonString: string, filePath: string): T
     {
-        // get json cache file and ensure it exists, create if it doesnt
-        const jsonCachePath = "./user/cache/jsonCache.json";
-        if (!this.jsonCacheExists)
-        {
-            
-            if (!this.vfs.exists(jsonCachePath))
-            {
-                this.vfs.writeFile(jsonCachePath, "{}");
-            }
-            this.jsonCacheExists = true;
-        }
-
+        this.ensureJsonCacheExists(this.jsonCachePath);
+        this.hydrateJsonCache(this.jsonCachePath);
 
         // Generate hash of string
         const generatedHash = this.hashUtil.generateSha1ForData(jsonString);
-
-        // Get all file hashes
-        if (!this.fileHashes)
-        {
-            this.fileHashes = this.deserialize(this.vfs.readFile(`${jsonCachePath}`));
-        }
 
         // Get hash of file and check if missing or hash mismatch
         let savedHash = this.fileHashes[filePath];
@@ -111,7 +157,7 @@ export class JsonUtil
                 {
                     // data valid, save hash and call function again
                     this.fileHashes[filePath] = generatedHash;
-                    this.vfs.writeFile(jsonCachePath, this.serialize(this.fileHashes, true));
+                    this.vfs.writeFile(this.jsonCachePath, this.serialize(this.fileHashes, true));
                     savedHash = generatedHash;
                 }
                 return data as T;
@@ -134,8 +180,44 @@ export class JsonUtil
         return this.deserialize<T>(jsonString);
     }
 
-    public clone<T>(data: T): T
+    
+    /**
+     * Create file if nothing found
+     * @param jsonCachePath path to cache
+     */
+    protected ensureJsonCacheExists(jsonCachePath: string): void
     {
-        return this.deserialize<T>(this.serialize(data));
+        if (!this.jsonCacheExists)
+        {
+            if (!this.vfs.exists(jsonCachePath))
+            {
+                // Create empty object at path
+                this.vfs.writeFile(jsonCachePath, "{}");
+            }
+            this.jsonCacheExists = true;
+        }
+    }
+
+    /**
+     * Read contents of json cache and add to class field
+     * @param jsonCachePath Path to cache
+     */
+    protected hydrateJsonCache(jsonCachePath: string) : void
+    {
+        // Get all file hashes
+        if (!this.fileHashes)
+        {
+            this.fileHashes = this.deserialize(this.vfs.readFile(`${jsonCachePath}`));
+        }
+    }
+
+    /**
+     * Convert into string and back into object to clone object
+     * @param objectToClone Item to clone
+     * @returns Cloned parameter
+     */
+    public clone<T>(objectToClone: T): T
+    {
+        return this.deserialize<T>(this.serialize(objectToClone));
     }
 }
