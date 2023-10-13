a3be4bbdf0963a0ada2eedd15183d0d5e39a5c1b
Refringe <brownelltyler@gmail.com>
2023-10-13 18:45:47 -0400

    Refactor of InsuranceController & New ItemHelper Methods
    This commit is my second go-around at refactoring the `InsuranceController`, attempting to improving the code's modularity, maintainability, and efficiency while squashing a few bugs along the way.
    
    1. **InsuranceController.ts**
        - Removed `ITemplateItem` import, as it is no longer used.
        - Introduced the `adoptOrphanedItems` method to manage orphaned items in the insurance list.
    	    - Since "normal" items are individually rolled for deletion, and can be nested within one another, there are situations where a parent item is deleted, leaving its children orphaned. This method moves those orphaned children from their missing parent into the root of the insurance container.
        - Overhauled `findItemsToDelete` method to improve its efficiency and readability:
            - Divided the original monolithic method into smaller, specialized methods like `populateItemsMap`, `populateParentAttachmentsMap`, `processRegularItems`, and `processAttachments`.
            - Changed the return type to `Set<string>` for better performance.
            - Introduced `EnrichedItem` interface (a simple extension of the `Item` interface) to add additional item data, like `name` and `maxPrice` to `Item` objects as they're processed throughout the class. This is done in place of repeatedly querying for this data, or complicating return types.
            - Enhanced logging capabilities to debug the item deletion process. Due to the *current* lack of testing available I've stepped up the amount of debug logging that is done. This will hopefully help us find issues in the future.
        - Modified the `rollForItemDelete` method, now renamed to `rollForDelete`, to include more detailed logging, return a boolean directly, and changed the `insuredItem` parameter to be optional.
        - Added new methods for dealing with some of the particulars that arise from item adoption and creating item maps.
        - Improved inline comments and documentation for better code maintainability.
    
    2. **ItemHelper.ts**
        - Added the `isRaidModdable` method to check if an item is *actually* modifiable in-raid, which takes into account not just the item, but the item that it's attached to.
        - Added the `getAttachmentMainParent` method to fetch the main parent item of a given attachment, useful for item hierarchy traversal. For example, if you pass it an item ID of a suppressor, it will traverse up the muzzle brake, barrel, upper receiver, and return the gun that the suppressor is ultimately attached to, even if that gun is located within other multiple containers.
        - Added the `isAttachmentAttached` method to check if an item is an attachment that is currently attached to its parent.
    
    **Fixes:**
     - Resolved an issue that caused item attachments from being property grouped together for deletion rolls. This issue prevented valuable attachments from being taken first.
     - Resolved an issue that caused child items being orphaned when their parent was removed due to an insurance roll. Probable cause of the bug that made the client spaz out and send repeated insurance packages to the profile---Though I'm still unable to reproduce.
     - Probably more. 😑

--- project/src/helpers/ItemHelper.ts
+++ project/src/helpers/ItemHelper.ts
@@ -760,6 +760,87 @@ class ItemHelper
         return false;
     }
 
+    /**
+     * Checks to see if the item is *actually* moddable in-raid. Checks include the items existence in the database, the
+     * parent items existence in the database, the existence (and value) of the items RaidModdable property, and that
+     * the parents slot-required property exists, matches that of the item, and it's value.
+     *
+     * Note: this function does not preform any checks to see if the item and parent are *actually* related.
+     *
+     * @param item The item to be checked
+     * @param parent The parent of the item to be checked
+     * @returns True if the item is actually moddable, false if it is not, and null if the check cannot be performed.
+     */
+    public isRaidModdable(item: Item, parent: Item): boolean | null
+    {
+        // This check requires the item to have the slotId property populated.
+        if (!item.slotId)
+        {
+            return null;
+        }
+
+        const itemTemplate = this.getItem(item._tpl);
+        const parentTemplate = this.getItem(parent._tpl);
+
+        // Check for RaidModdable property on the item template.
+        let isNotRaidModdable = false;
+        if (itemTemplate[0])
+        {
+            isNotRaidModdable = itemTemplate[1]?._props?.RaidModdable === false;
+        }
+
+        // Check to see if the slot that the item is attached to is marked as required in the parent item's template.
+        let isRequiredSlot = false;
+        if (parentTemplate[0] && parentTemplate[1]?._props?.Slots)
+        {
+            isRequiredSlot = parentTemplate[1]._props.Slots.some(slot => slot._name === item.slotId && slot._required);
+        }
+
+        return itemTemplate[0] && parentTemplate[0] && !(isNotRaidModdable || isRequiredSlot);
+    }
+
+    /**
+     * Retrieves the main parent item for a given attachment item.
+     *
+     * This method traverses up the hierarchy of items starting from a given `itemId`, until it finds the main parent
+     * item that is not an attached attachment itself. In other words, if you pass it an item id of a suppressor, it
+     * will traverse up the muzzle brake, barrel, upper receiver, and return the gun that the suppressor is ultimately
+     * attached to, even if that gun is located within multiple containers.
+     *
+     * It's important to note that traversal is expensive, so this method requires that you pass it a Map of the items
+     * to traverse, where the keys are the item IDs and the values are the corresponding Item objects. This alleviates
+     * some of the performance concerns, as it allows for quick lookups of items by ID.
+     *
+     * To generate the map:
+     * ```
+     * const itemsMap = new Map<string, Item>();
+     * items.forEach(item => itemsMap.set(item._id, item));
+     * ```
+     *
+     * @param itemId - The unique identifier of the item for which to find the main parent.
+     * @param itemsMap - A Map containing item IDs mapped to their corresponding Item objects for quick lookup.
+     * @returns The Item object representing the top-most parent of the given item, or `null` if no such parent exists.
+     */
+    public getAttachmentMainParent(itemId: string, itemsMap: Map<string, Item>): Item | null
+    {
+        let currentItem = itemsMap.get(itemId);
+        while (currentItem && this.isAttachmentAttached(currentItem))
+        {
+            currentItem = itemsMap.get(currentItem.parentId);
+        }
+        return currentItem;
+    }
+
+    /**
+     * Determines if an item is an attachment that is currently attached to it's parent item.
+     *
+     * @param item The item to check.
+     * @returns true if the item is attached attachment, otherwise false.
+     */
+    public isAttachmentAttached(item: Item): boolean
+    {
+        return item.slotId !== "hideout" && item.slotId !== "main" && isNaN(Number(item.slotId));
+    }
 
     /**
      * Get the inventory size of an item
