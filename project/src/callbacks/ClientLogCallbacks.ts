462f7014a00c2cb66eb29eacb50e9f4d4c87d948
Cj <161484149+CJ-SPT@users.noreply.github.com>
2024-03-06 23:27:27 -0500

    Implement request for beta/changelog

--- project/src/callbacks/ClientLogCallbacks.ts
+++ project/src/callbacks/ClientLogCallbacks.ts
@@ -1,6 +1,9 @@
 import { ClientLogController } from "@spt-aki/controllers/ClientLogController";
 import { INullResponseData } from "@spt-aki/models/eft/httpResponse/INullResponseData";
+import { ConfigTypes } from "@spt-aki/models/enums/ConfigTypes";
+import { ICoreConfig, IRelease } from "@spt-aki/models/spt/config/ICoreConfig";
 import { IClientLogRequest } from "@spt-aki/models/spt/logging/IClientLogRequest";
+import { ConfigServer } from "@spt-aki/servers/ConfigServer";
 import { HttpResponseUtil } from "@spt-aki/utils/HttpResponseUtil";
 import { inject, injectable } from "tsyringe";
 
@@ -11,6 +14,7 @@ export class ClientLogCallbacks
     constructor(
         @inject("HttpResponseUtil") protected httpResponse: HttpResponseUtil,
         @inject("ClientLogController") protected clientLogController: ClientLogController,
+        @inject("ConfigServer") protected configServer: ConfigServer,
     )
     {}
 
@@ -21,5 +25,14 @@ export class ClientLogCallbacks
     {
         this.clientLogController.clientLog(info);
         return this.httpResponse.nullResponse();
+    }
+
+    /**
+     * Handle /singleplayer/release
+     */
+    public releaseNotes(): string
+    {
+        const data: IRelease = this.configServer.getConfig<ICoreConfig>(ConfigTypes.CORE).release;
+        return this.httpResponse.noBody(data);
     }
 }
