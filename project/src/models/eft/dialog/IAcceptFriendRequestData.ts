376791db18f932e38aebfba04c2635537525dfe8
TheSparta <thesparta@noreply.dev.sp-tarkov.com>
2024-05-03 23:45:59 +0100

    Added missing endpoints
    - Server now handles `/client/friend/request/decline` and `/client/friend/request/accept-all`

--- project/src/models/eft/dialog/IAcceptFriendRequestData.ts
+++ project/src/models/eft/dialog/IAcceptFriendRequestData.ts
@@ -6,6 +6,10 @@ export interface ICancelFriendRequestData extends IBaseFriendRequest
 {
 }
 
+export interface IDeclineFriendRequestData extends IBaseFriendRequest
+{
+}
+
 export interface IBaseFriendRequest
 {
     profileId: string;
