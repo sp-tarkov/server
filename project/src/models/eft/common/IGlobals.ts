308213bd7f14b2193a8747fbd315ad103d7b3f6e
DenisZhamkin <den.zhmakin.00@gmail.com>
2024-09-15 10:31:20 +0300

    feat: added the ability to enable insurance behavior as in live eft

--- project/src/models/eft/common/IGlobals.ts
+++ project/src/models/eft/common/IGlobals.ts
@@ -96,6 +96,7 @@ export interface IConfig {
     SkillEnduranceWeightThreshold: number;
     TeamSearchingTimeout: number;
     Insurance: IInsurance;
+    IsPveInsurance: boolean;
     SkillExpPerLevel: number;
     GameSearchingTimeout: number;
     WallContusionAbsorption: Ixyz;
