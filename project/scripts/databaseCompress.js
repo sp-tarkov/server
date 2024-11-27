ff364c83231285ca0fae42ee1f1e18a76d9730a6
Refringe <me@refringe.com>
2024-11-27 12:41:37 -0500

    NPM Compression Scripts
    Adds two NPM scripts to handle compressing and decompressing the locations data in a reproducible way.
    
    Needs to be tested on Windows.

---
+++ project/scripts/databaseCompress.js
@@ -0,0 +1,31 @@
+// This script compresses the locations database into a 7z archive.
+
+const Seven = require("node-7z");
+const path = require("node:path");
+const { path7za } = require("7zip-bin");
+
+const archivePath = path.resolve(__dirname, "../assets/compressed/locations.7z");
+const locationsDir = path.resolve(__dirname, "../assets/database/locations/*");
+
+let hadError = false;
+
+console.log(archivePath);
+console.log(locationsDir);
+
+const myStream = Seven.add(archivePath, locationsDir, {
+    recursive: true,
+    $bin: path7za,
+    method: ["0=LZMA2"],
+    compressionLevel: 9,
+});
+
+myStream.on("end", () => {
+    if (!hadError) {
+        console.log("Compression completed successfully.");
+    }
+});
+
+myStream.on("error", (err) => {
+    hadError = true;
+    console.error(`Error compressing locations: ${err}`);
+});
