0faa548d3804335c8aacaeb0e418583cffa7fdca
Archangel <jesse@archangel.wtf>
2024-12-30 16:45:47 +0100

    Export SPTWebSocket

--- project/src/servers/ws/SPTWebsocket.ts
+++ project/src/servers/ws/SPTWebsocket.ts
@@ -1,6 +1,6 @@
 import WebSocket from "ws";
 
-class SPTWebSocket extends WebSocket {
+export class SPTWebSocket extends WebSocket {
     // biome-ignore lint/suspicious/noExplicitAny: Any is required here, I dont see any other way considering it will complain if we use BufferLike
     public sendAsync(ws: WebSocket, data: any): Promise<void> {
         return new Promise((resolve, reject) => {
