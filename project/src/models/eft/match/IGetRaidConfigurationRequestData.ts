7067d5f58a44d44bde8530e3d4abb8ce0894dcf0
Chomp <dev@dev.sp-tarkov.com>
2025-04-02 11:39:25 +0100

    Updated `IGetRaidConfigurationRequestData`

--- project/src/models/eft/match/IGetRaidConfigurationRequestData.ts
+++ project/src/models/eft/match/IGetRaidConfigurationRequestData.ts
@@ -1,6 +1,7 @@
 import { IRaidSettings } from "@spt/models/eft/match/IRaidSettings";
 
 export interface IGetRaidConfigurationRequestData extends IRaidSettings {
-    keyId: string;
+    onlinePveRaidStates: Record<string, boolean>;
+    transitionType: number;
     MaxGroupCount: number;
 }
